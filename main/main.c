6eef1911557cefacc5d409a86e10a6156ead8a3d
Kiran De Roy <kiran@litrik.com>
2024-02-25 17:00:26 +0100

    RMT stepper move function works.

--- main/main.c
+++ main/main.c
@@ -11,30 +11,122 @@
 #include "esp_log.h"
 #include "stepper_motor_encoder.h"
 
-#define STEP_MOTOR_GPIO_SLP       GPIO_NUM_36
-#define STEP_MOTOR_GPIO_DIR      GPIO_NUM_38
 #define STEP_MOTOR_GPIO_STEP     GPIO_NUM_37
-#define STEP_MOTOR_GPIO_RST     GPIO_NUM_35
-#define STEP_MOTOR_SLEEP_LEVEL  1 // DRV8825 is enabled on low level
-#define STEP_MOTOR_SPIN_DIR_CLOCKWISE 0
-#define STEP_MOTOR_SPIN_DIR_COUNTERCLOCKWISE !STEP_MOTOR_SPIN_DIR_CLOCKWISE
+
+#define STEP_MOTOR_SLP1       GPIO_NUM_36
+#define STEP_MOTOR_DIR1      GPIO_NUM_38
+#define STEP_MOTOR_RST1     GPIO_NUM_35
+
+#define STEP_MOTOR_SLP2       GPIO_NUM_21
+
+#define STEP_MOTOR_DIR2      GPIO_NUM_48
+#define STEP_MOTOR_RST2     GPIO_NUM_45
+
+#define GPIO_OUTPUT_PIN_SEL  ( (1ULL<<STEP_MOTOR_DIR1)     \
+| (1ULL<<STEP_MOTOR_SLP1)     \
+| (1ULL<<STEP_MOTOR_RST1)     \
+| (1ULL<<STEP_MOTOR_DIR2)     \
+| (1ULL<<STEP_MOTOR_SLP2)     \
+| (1ULL<<STEP_MOTOR_RST2))
+
+#define EMERGENCY_OUTER    GPIO_NUM_5
+#define EMERGENCY_INNER    GPIO_NUM_7
+
+#define GPIO_INPUT_PIN_SEL \
+((1ULL<<EMERGENCY_OUTER) \
+| (1ULL<<EMERGENCY_INNER))
+
+#define ORTHOGONAL_TILE_IN_STEPS 5626
+#define DIAGONAL_TILE_IN_STEPS 7956
 
 #define STEP_MOTOR_RESOLUTION_HZ 1000000 // 1MHz resolution
+#define TAG_RMT "RMT"
 
-static const char *TAG = "example";
+const static uint32_t uniform_speed_hz = 10000;
 
-void app_main(void)
-{
-    ESP_LOGI(TAG, "Initialize EN + DIR GPIO");
-    gpio_config_t en_dir_gpio_config = {
-            .mode = GPIO_MODE_OUTPUT,
-            .intr_type = GPIO_INTR_DISABLE,
-            .pin_bit_mask = 1ULL << STEP_MOTOR_GPIO_DIR | 1ULL << STEP_MOTOR_GPIO_SLP | 1ULL << STEP_MOTOR_GPIO_RST,
+typedef enum {
+    N = 0, S = 1, W = 2, E = 3, NE = 4, NW = 5, SW = 6, SE = 7
+} Direction;
+
+static const int dirConfigs[8][4] = {{1, 1, 1, 1}, // N
+                                     {0, 0, 1, 1}, // S
+                                     {0, 1, 1, 1}, // W
+                                     {1, 0, 1, 1}, // E
+                                     {1, 0, 1, 0}, // NE
+                                     {0, 1, 0, 1}, // NW
+                                     {0, 0, 1, 0}, // SW
+                                     {0, 0, 0, 1}};// SE
+
+rmt_channel_handle_t motor_chan = NULL;
+rmt_encoder_handle_t uniform_motor_encoder = NULL;
+
+void disableMotor1() {
+    gpio_set_level(STEP_MOTOR_RST1, 0);
+    gpio_set_level(STEP_MOTOR_SLP1, 0);
+}
+
+void disableMotor2() {
+    gpio_set_level(STEP_MOTOR_RST2, 0);
+    gpio_set_level(STEP_MOTOR_SLP2, 0);
+}
+
+void enableMotor1() {
+    gpio_set_level(STEP_MOTOR_RST1, 1);
+    gpio_set_level(STEP_MOTOR_SLP1, 1);
+}
+
+void enableMotor2() {
+    gpio_set_level(STEP_MOTOR_RST2, 1);
+    gpio_set_level(STEP_MOTOR_SLP2, 1);
+}
+
+void toggleMotor(bool switchOn, int motor) {
+    if (motor == 1) {
+        if (switchOn) {
+            enableMotor1();
+        } else {
+            disableMotor1();
+        }
+    } else if(motor == 2){
+        if (switchOn) {
+            enableMotor2();
+        } else {
+            disableMotor2();
+        }
+    }
+}
+
+int move(Direction dir, int numHalfTiles ){
+    int tileDistance;
+    if(dir > 3){
+        tileDistance = (DIAGONAL_TILE_IN_STEPS/2)*numHalfTiles;
+    } else {
+        tileDistance = (ORTHOGONAL_TILE_IN_STEPS/2)*numHalfTiles;
+    }
+
+    gpio_set_level(STEP_MOTOR_DIR1, dirConfigs[dir][0]);
+    gpio_set_level(STEP_MOTOR_DIR2, dirConfigs[dir][1]);
+    toggleMotor(dirConfigs[dir][2], 1);
+    toggleMotor(dirConfigs[dir][3], 2);
+    printf("dirConfigs %i : DIR1 = %i, DIR2 = %i, M1  = %i, M2  = %i \n", dir, dirConfigs[dir][0], dirConfigs[dir][1], dirConfigs[dir][2], dirConfigs[dir][3]);
+
+    ESP_LOGI(TAG_RMT, "Spin motor for 6000 steps: 500 accel + 5000 uniform + 500 decel");
+    rmt_transmit_config_t tx_config = {
+            .loop_count = tileDistance,
     };
-    ESP_ERROR_CHECK(gpio_config(&en_dir_gpio_config));
 
-    ESP_LOGI(TAG, "Create RMT TX channel");
-    rmt_channel_handle_t motor_chan = NULL;
+    // uniform phase
+    ESP_ERROR_CHECK(rmt_transmit(motor_chan, uniform_motor_encoder,
+                                 &uniform_speed_hz, sizeof(uniform_speed_hz), &tx_config));
+
+    // wait all transactions finished
+    ESP_ERROR_CHECK(rmt_tx_wait_all_done(motor_chan, -1));
+
+    return tileDistance;
+}
+
+void setupRMT(){
+    ESP_LOGI(TAG_RMT, "Create RMT TX channel");
     rmt_tx_channel_config_t tx_chan_config = {
             .clk_src = RMT_CLK_SRC_DEFAULT, // select clock source
             .gpio_num = STEP_MOTOR_GPIO_STEP,
@@ -44,46 +136,36 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &motor_chan));
 
-    ESP_LOGI(TAG, "Set spin direction");
-    gpio_set_level(STEP_MOTOR_GPIO_DIR, STEP_MOTOR_SPIN_DIR_CLOCKWISE);
-    ESP_LOGI(TAG, "Enable step motor");
-    gpio_set_level(STEP_MOTOR_GPIO_SLP, STEP_MOTOR_SLEEP_LEVEL);
-    gpio_set_level(STEP_MOTOR_GPIO_RST, 1);
-
     stepper_motor_uniform_encoder_config_t uniform_encoder_config = {
             .resolution = STEP_MOTOR_RESOLUTION_HZ,
     };
-    rmt_encoder_handle_t uniform_motor_encoder = NULL;
+
     ESP_ERROR_CHECK(rmt_new_stepper_motor_uniform_encoder(&uniform_encoder_config, &uniform_motor_encoder));
 
-    ESP_LOGI(TAG, "Enable RMT channel");
+    ESP_LOGI(TAG_RMT, "Enable RMT channel");
     ESP_ERROR_CHECK(rmt_enable(motor_chan));
+}
 
-    ESP_LOGI(TAG, "Spin motor for 6000 steps: 500 accel + 5000 uniform + 500 decel");
-    rmt_transmit_config_t tx_config = {
-            .loop_count = 0,
+void app_main(void) {
+    ESP_LOGI(TAG_RMT, "Initialize EN + DIR GPIO");
+    gpio_config_t en_dir_gpio_config = {
+            .mode = GPIO_MODE_OUTPUT,
+            .intr_type = GPIO_INTR_DISABLE,
+            .pin_bit_mask = GPIO_OUTPUT_PIN_SEL
     };
 
-    const static uint32_t uniform_speed_hz = 10000;
+//    en_dir_gpio_config.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+//    en_dir_gpio_config.mode = GPIO_MODE_INPUT;
+//    en_dir_gpio_config.pull_up_en = 1;
+//    gpio_config(&en_dir_gpio_config);
 
-    while (1) {
-        // acceleration phase
-//        tx_config.loop_count = 0;
-//        ESP_ERROR_CHECK(rmt_transmit(motor_chan, accel_motor_encoder, &accel_samples, sizeof(accel_samples), &tx_config));
+    ESP_ERROR_CHECK(gpio_config(&en_dir_gpio_config));
 
-        // uniform phase
-        tx_config.loop_count = 5000;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, uniform_motor_encoder,
-                                     &uniform_speed_hz, sizeof(uniform_speed_hz), &tx_config));
+    setupRMT();
 
-//        // deceleration phase
-//        tx_config.loop_count = 0;
-//        ESP_ERROR_CHECK(rmt_transmit(motor_chan, decel_motor_encoder, &decel_samples, sizeof(decel_samples), &tx_config));
-        // wait all transactions finished
-        ESP_ERROR_CHECK(rmt_tx_wait_all_done(motor_chan, -1));
+    move(NE, 4);
+    move(SE, 4);
+    move(SW, 4);
+    move(NW, 4);
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        gpio_set_level(STEP_MOTOR_GPIO_DIR, !STEP_MOTOR_SPIN_DIR_CLOCKWISE);
-
-    }
 }
